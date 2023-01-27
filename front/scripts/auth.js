14b1b435c14013cc519e2d494eb459f73bddbf48
Evgenij Ryabko <jeniaryanko@mail.ru>
2023-01-27 13:45:36 +0300

    FrontAuth

--- front/scripts/auth.js
+++ front/scripts/auth.js
@@ -1,7 +1,62 @@
-function login(){
-    const resText = document.getElementById('responseText');
-    axios
-        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        .then(response => (resText.innerHTML = response.data))
-        .catch(error => console.log(error));;
+async function login(){
+    //Authentication values
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    //Div that shows auth errors
+    const errorDiv = document.getElementById('errorDiv');
+
+    //Trying to post data to server
+    try{
+        const res = await axios({
+            method: 'post',
+            url: `http://localhost:10001/api/auth/login`,
+            data: {
+                email: email,
+                password: password
+            }
+        });
+
+        //Show response token
+        alert(res.data.token);
+        window.location.href='./home.html';
+    } catch(e) {
+        const error = new Error(e.response.data.error);
+        const status = e.response.status;
+
+        errorDiv.classList.remove('error_hidden');
+        errorDiv.classList.add('error_show');
+        errorDiv.innerHTML = `<p>Error${status}: ${error.message}</p>`;
+    }
+}
+
+async function register(){
+    //Authentication values
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    //Div that shows auth errors
+    const errorDiv = document.getElementById('errorDiv');
+
+    //Trying to post data to server
+    try{
+        const res = await axios({
+            method: 'post',
+            url: `http://localhost:10001/api/auth/register`,
+            data: {
+                email: email,
+                password: password
+            }
+        });
+
+        console.log(res);
+        alert('Success!');
+    } catch(e) {
+        const error = new Error(e.response.data.error);
+        const status = e.response.status;
+
+        errorDiv.classList.remove('error_hidden');
+        errorDiv.classList.add('error_show');
+        errorDiv.innerHTML = `<p>Error${status}: ${error.message}</p>`;
+    }
 }
